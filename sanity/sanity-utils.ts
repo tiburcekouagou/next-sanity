8d3987703c3f127276a91ad0195f062833313def
Tiburce Kouagou <kouagoutiburce@gmail.com>
2024-01-15 01:04:12 +0100

    added pages documents

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,4 +1,5 @@
 import clientConfig from "@/sanity/config/client-config";
+import { Page } from "@/types/Page";
 import { Project } from "@/types/Projects";
 import { createClient, groq } from "next-sanity";
 
@@ -27,6 +28,30 @@ export async function getProject(slug: string): Promise<Project> {
             url,
             content
         }`,
+    { slug }
+  );
+}
+
+export async function getPages(): Promise<Page[]> {
+  return createClient(clientConfig).fetch(
+    groq`*[_type == "page"]{
+    _id,
+    _createdAt,
+    title,
+    "slug": slug.current,
+  }`
+  );
+}
+
+export async function getPage(slug: string): Promise<Page> {
+  return createClient(clientConfig).fetch(
+    groq`*[_type == "page" && slug.current == $slug][0]{
+      _id,
+      _createdAt,
+      title,
+      "slug": slug.current,
+      content
+    }`,
     { slug }
   );
 }
---
+++ types/Page.ts
@@ -0,0 +1,8 @@
+export type Page = {
+  _id: string;
+  _title: string;
+  _createdAt: string;
+  title: string;
+  slug: string;
+  content: any[];
+};
